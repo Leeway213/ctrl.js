eb587062ee4636ea56fd32ddb8ee4860081cf630
leeway.zlw <leeway.zlw@alibaba-inc.com>
2020-05-26 10:47:07 +0800

    add playground

---
+++ playground/js/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable prefer-const */
+
+window.onload = function() {
+  if (window.CTRLJS) {
+    console.log(window.CTRLJS);
+  }
+};
+
+
